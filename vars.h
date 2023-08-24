54a524964e35a9ad1b96a6c01bc2f8bf16d350a6
bhumphrey0x20 <38211805+bhumphrey0x20@users.noreply.github.com>
2023-08-24 11:55:21 -0500

    Add files via upload

---
+++ vars.h
@@ -0,0 +1,15 @@
+#ifndef _VARS_H
+#define _VARS_H
+
+const char *text_code = "Message Code: ";
+const char *text_msg_num = "Message No.: ";
+const char *text_dims = "Dimenstions: ";
+const char *text_w    = "Width: "; 
+const char *text_h    = "Height: "; 
+const char *text_tot_size = "Total Size: ";
+const char *text_img_size = "Image Size: ";
+
+
+
+
+#endif
